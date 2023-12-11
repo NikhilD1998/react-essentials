7e12e0267d36d63f3cf03cf939d5d10ebf0bd733
NikhilD1998 <nikhildadab@hotmail.com>
2023-12-11 11:33:49 +0530

    Rendering Content Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import { useState } from "react";
 import CoreConcept from "./components/CoreConcepts";
 import Header from "./components/Header/Header";
 import TabButton from "./components/TabButton";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 
 function App() {
-  const [selctedTopic, setSelectedTopic] = useState("Please click a button");
+  const [selectedTopic, setSelectedTopic] = useState("");
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -37,7 +51,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selctedTopic}
+          {tabContent}
         </section>
       </main>
     </div>
